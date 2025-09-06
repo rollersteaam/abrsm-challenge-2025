8aaefec16de4218a62d0c57f705681d6cbb67ad7
rollersteaam <rollersteaam@gmail.com>
2025-09-06 16:34:20 +0100

    feat: load model into webapp

--- backend/feedback/views.py
+++ backend/feedback/views.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
 
+import torch
 from django import forms
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest, JsonResponse
 from django.urls import path
 from django.core.files import File
 from django.views.decorators.csrf import csrf_exempt
 
+from .audio_model import combined_model
 
 @dataclass
 class Piece():
@@ -53,5 +55,12 @@ def feedback_view(request):
 def get_feedback_from_pieces(piece_1: Piece, piece_2: Piece) -> Feedback:
     print(f"Piece 1: {piece_1.name} ({piece_1.file.size / 1000}kb)")
     print(f"Piece 2: {piece_2.name} ({piece_2.file.size / 1000}kb)")
+
+    spec_size = 1500
+    dim = 64
+    drop = 0.3
+    in_channels_class = 128
+    model = combined_model.combined_model(dim, drop, in_channels_class, spec_size)
+    model.load_state_dict(torch.load("./feedback/audio_model/model_epoch_13.pt"))
 
     return Feedback(50, "No feedback")
