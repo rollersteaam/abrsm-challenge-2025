5705d5cb0670a703acbb18a5b9419a5524cd9ec5
rollersteaam <rollersteaam@gmail.com>
2025-09-06 18:34:11 +0100

    feat: training from chatgpt

--- backend/feedback/urls.py
+++ backend/feedback/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from feedback.views import feedback_view
+from feedback.views import feedback_view, train_view
 
 
 urlpatterns = [
-    path("", feedback_view)
+    path("", feedback_view),
+    path("train/", train_view)
 ]
