c95498971ff494185fa32906cae9e8c16a044dc8
JamesBolt22 <j.g.bolt@qmul.ac.uk>
2025-09-07 14:04:06 +0100

    Final model files

--- model/combined_model.py
+++ model/combined_model.py
@@ -14,8 +14,13 @@ class combined_model(nn.Module):
         self.mark_linear1 = nn.Linear(self.emb_size+1, 512)
         self.mark_linear2 = nn.Linear(512, 256)
         self.mark_linear3 = nn.Linear(256, 40)
+
+        self.word_vec_size = 768
+        self.word_linear1 = nn.Linear(self.emb_size+1, 2000)
+        self.word_linear2 = nn.Linear(2000, self.word_vec_size*5)
+
         self.dropout = nn.Dropout(drop)
-        
+
     def forward(self, y):
 
         #X Shape = (B,C,H,W)
@@ -27,4 +32,12 @@ class combined_model(nn.Module):
         mark_vector = self.dropout(mark_vector)
         mark_vector = F.gelu(mark_vector)
         mark_vector = self.mark_linear3(mark_vector)
-        return mark_vector
+
+        word_vector = nn.Flatten()(y)
+        word_vector = self.word_linear1(word_vector)
+        word_vector = self.dropout(word_vector)
+        word_vector = F.gelu(word_vector)
+        word_vector = self.word_linear2(word_vector)
+        word_vector = word_vector.view(-1, 5, self.word_vec_size)
+
+        return mark_vector, word_vector
