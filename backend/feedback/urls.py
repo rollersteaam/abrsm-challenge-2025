df6300fa1e66d376286f4b826a11e682a17ea9fe
rollersteaam <rollersteaam@gmail.com>
2025-09-07 11:07:37 +0100

    feat: clean chatgpt data

--- backend/feedback/urls.py
+++ backend/feedback/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from feedback.views import feedback_view, train_view
+from feedback.views import feedback_view, train_view, clean_view
 
 
 urlpatterns = [
     path("", feedback_view),
-    path("train/", train_view)
+    path("train/", train_view),
+    path("clean/", clean_view)
 ]
